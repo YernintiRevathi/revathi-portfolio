aeb6d3dfe595396ba304f5fde4b08333ed8b6fee
Yerninti Revathi <reeshmayerninti@gmail.com>
2025-09-11 23:42:30 +0530

    Updated Experience.tsx

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -236,7 +236,7 @@ export default Experience;```
 //             //<Card key={index} className={exp.current ? 'border-primary shadow-lg' : ''}>
 //             <Card
 //                   key={index}
-//                   className={`h-full border border-primary shadow-sm transform transition-transform duration-300 hover:-translate-y-2 hover:scale-102 hover:shadow-2xl`}
+//                   className={\`h-full border border-primary shadow-sm transform transition-transform duration-300 hover:-translate-y-2 hover:scale-102 hover:shadow-2xl`}
 //             >
 //               <CardHeader>
 //                 <div className="flex flex-col lg:flex-row lg:items-start lg:justify-between gap-4">
