a57b9272c98acad9e90994f8044de5a471f4c7d5
Yerninti Revathi <reeshmayerninti@gmail.com>
2025-09-11 23:12:06 +0530

    Updated Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -22,7 +22,7 @@ const Index = () => {
                 <img
                   src="/profile.jpg" // Your image path
                   alt="Revathi Yerninti"
-                  className="w-60 h-60 md:w-48 md:h-48 rounded-full shadow-lg object-cover object-top" 
+                  className="w-32 h-40 md:w-48 md:h-48 rounded-full shadow-lg object-cover object-top" 
                 />
               </div>
 
